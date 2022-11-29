488efcd3d8bfe49fb85246e42612e3c58aeaabe7
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-11-29 17:17:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+document.querySelector("button").addEventListener("click", rollDice())
+
+function rollDice() {
+    let diceNumber = Math.floor(Math.random() * 6)
+    if (diceNumber = 0) {
+        document.querySelector("img1").setAttribute(".img1", dice1.png)
+    } else if {
+
+    }
+} 
